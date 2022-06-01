2e82d6ce28621e3b8fbbf4f806ff39ec28927bd3
hanwenma <1768263460@qq.com>
2022-06-01 21:03:01 +0800

    feat(binary-search-tree): binary search tree for k

--- code/tree/binary-search-tree.ts
+++ code/tree/binary-search-tree.ts
@@ -56,30 +56,42 @@ export function preOrederTraverse(
  * @param sequence
  * @returns sequence
  */
- export function inOrederTraverse(
-    tree: IBinaryTree,
-    sequence: number[] = []
-  ): number[] {
-    if (tree === null) return;
-    inOrederTraverse(tree.left, sequence);
-    sequence.push(tree.value);
-    inOrederTraverse(tree.right, sequence);
-    return sequence;
-  }
+export function inOrederTraverse(
+  tree: IBinaryTree,
+  sequence: number[] = []
+): number[] {
+  if (tree === null) return;
+  inOrederTraverse(tree.left, sequence);
+  sequence.push(tree.value);
+  inOrederTraverse(tree.right, sequence);
+  return sequence;
+}
 
-  /**
+/**
  * 后序遍历
  * @param tree
  * @param sequence
  * @returns sequence
  */
- export function postOrederTraverse(
-    tree: IBinaryTree,
-    sequence: number[] = []
-  ): number[] {
-    if (tree === null) return;
-    postOrederTraverse(tree.left, sequence);
-    postOrederTraverse(tree.right, sequence);
-    sequence.push(tree.value);
-    return sequence;
-  }
+export function postOrederTraverse(
+  tree: IBinaryTree,
+  sequence: number[] = []
+): number[] {
+  if (tree === null) return;
+  postOrederTraverse(tree.left, sequence);
+  postOrederTraverse(tree.right, sequence);
+  sequence.push(tree.value);
+  return sequence;
+}
+
+/**
+ * 二叉搜索树 - 查找第 k 小值
+ * 二叉搜索树特点：left <= value; right >= value;
+ * @param tree 
+ * @param k 
+ */
+export function binarySearchTree(tree: IBinaryTree, k: number): number | null {
+  const result: number[] = [];
+  inOrederTraverse(tree, result);
+  return result[k-1]||null;
+}
--- test/tree/binary-search-tree.test.js
+++ test/tree/binary-search-tree.test.js
@@ -3,20 +3,28 @@ import {
   preOrederTraverse,
   inOrederTraverse,
   postOrederTraverse,
+  binarySearchTree,
 } from "../../code/tree/binary-search-tree";
 
 describe("binary search tree", () => {
-  const tree = createBinaryTree([1, 2, 3, 4, 5]);
+  const tree1 = createBinaryTree([1, 2, 3, 4, 5]);
+  const tree2 = createBinaryTree([5, 3, 4, 1, 3]);
 
   test("pre oreder traverse", () => {
-    expect(preOrederTraverse(tree)).toEqual([1, 2, 4, 5, 3]);
+    expect(preOrederTraverse(tree1)).toEqual([1, 2, 4, 5, 3]);
   });
 
   test("in oreder traverse", () => {
-    expect(inOrederTraverse(tree)).toEqual([4, 2, 5, 1, 3]);
+    expect(inOrederTraverse(tree1)).toEqual([4, 2, 5, 1, 3]);
   });
 
   test("post oreder traverse", () => {
-    expect(postOrederTraverse(tree)).toEqual([4, 5, 2, 3, 1]);
+    expect(postOrederTraverse(tree1)).toEqual([4, 5, 2, 3, 1]);
+  });
+
+  test("binary search traverse for k", () => {
+    expect(binarySearchTree(tree2, 3)).toBe(3);
+    expect(binarySearchTree(tree2, 100)).toBe(null);
+    expect(binarySearchTree(tree2, 0)).toBe(null);
   });
 });
